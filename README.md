baf93b6807a6e3cb153f03a2c51725376e0f7992
Кузырятский Александр <a.kuzyryatskiy@timeweb.ru>
2026-05-21 10:13:58 +0200

    example repo

---
+++ README.md
@@ -0,0 +1,4 @@
+# app-example-bun-hapi
+
+Пример приложения Hapi на Bun для тестов Timeweb Cloud App Platform.
+
---
+++ index.js
@@ -0,0 +1,21 @@
+﻿'use strict';
+const Hapi = require('@hapi/hapi');
+const port = Number(process.env.PORT || 3000);
+const init = async () => {
+  const server = Hapi.server({
+    port,
+    host: '0.0.0.0',
+  });
+  server.route({
+    method: 'GET',
+    path: '/',
+    handler: () => 'Timeweb Cloud + Hapi on Bun = тЭдя╕П',
+  });
+  await server.start();
+  console.log('Hapi server is running on %s', server.info.uri);
+};
+process.on('unhandledRejection', (err) => {
+  console.error(err);
+  process.exit(1);
+});
+init();
